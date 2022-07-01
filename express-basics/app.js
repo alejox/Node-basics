ba238ee78741642208609eada944a7d1efa4dee9
alejox <alejoxdrums@gmail.com>
2022-07-01 00:15:12 -0500

    Async

--- express-basics/app.js
+++ express-basics/app.js
@@ -1,35 +1,56 @@
 const expres = require("express");
+const { Sequelize, DataTypes } = require("sequelize");
 
-//Datos Dummy
-const users = [
-  {
-    id: 1,
-    name: "Max",
-  },
-  {
-    id: 2,
-    name: "Alejandro",
-  },
-  {
-    id: 3,
-    name: "Fernando",
-  },
-];
+//Conect to database
+const db = new Sequelize({
+	dialect:'postgres',
+	host:'localhost',
+	username:'postgres',
+	password:'pass1234',
+	port:5432,
+	database:'DB'
+})
 
-const posts = [
-  {
-    id: 1,
-    title: "Post #1",
-  },
-  {
-    id: 2,
-    title: "Post #2",
-  },
-  {
-    id: 3,
-    title: "Post #3",
-  },
-];
+db.authenticate()
+.then(()=> console.log('Db autehticated'))
+.catch(err => console.log(err));
+
+db.sync()
+.then(()=> console.log('Db Synced'))
+.catch(err => console.log(err));
+
+//Create our first model (table)
+
+const User = db.define('user', {
+	id: {
+		primaryKey: true,
+		type: DataTypes.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+	},
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	age: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	email: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: true,
+	},
+	password: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	status: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		defaultValue: 'active',
+	},
+});
 
 //Init express app
 const app = expres();
@@ -38,50 +59,57 @@ const app = expres();
 app.use(expres.json());
 
 //Define endpoints
-app.get('/users', (req, res)=>{
-	//Process the request(Return the list of users)
-	res.status(200).json({
-		status:'success',
-		users,
-	});
-});
+app.get("/users", async(req, res) => {
 
-app.post('/users', (req, res)=>{
-	const {name} = req.body;
-
-	const newUser ={
-		id:Math.floor(Math.random() * 1000),
-		name,
+	try{
+		const users = await User.findAll()
+		//Process the request(Return the list of users)
+		res.status(200).json({
+			status: "success",
+			users,
+		});
+	}catch(err){
+		console.log(err);
 	}
 
-	users.push(newUser);
-	res.status(201).json({
-	status:"success",
-	newUser
-});
 });
 
-app.get('/posts', (req,res) =>{
-	res.status(200).json({
-		status:"success",
-		posts
-	});
+app.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  const newUser = {
+    id: Math.floor(Math.random() * 1000),
+    name,
+  };
+
+  users.push(newUser);
+  res.status(201).json({
+    status: "success",
+    newUser,
+  });
 });
 
-app.post('/posts', (req,res)=>{
-	const {title} = req.body;
+app.get("/posts", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    posts,
+  });
+});
 
-	const newPost ={
-		id:Math.floor(Math.random() * 1000),
-		title,
-	}
+app.post("/posts", (req, res) => {
+  const { title } = req.body;
 
-	posts.push(newPost);
-	res.status(201).json({
-		status:"success",
-		newPost
-	})
-})
+  const newPost = {
+    id: Math.floor(Math.random() * 1000),
+    title,
+  };
+
+  posts.push(newPost);
+  res.status(201).json({
+    status: "success",
+    newPost,
+  });
+});
 
 app.listen(4000, () => {
   console.log("Express app running!!!");
